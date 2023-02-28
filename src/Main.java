81751f7304562d7bbec8f92ea9fa2766e0661b18
Ruslan <sonikx94@yandex.ru>
2023-02-28 23:19:45 +0300

    Toy store

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ToyStore toyStore = new ToyStore();
+
+        try {
+            toyStore.addNewToy("Человек-паук");
+            toyStore.addNewToy("Плюшевый медведь", 200);
+            toyStore.addNewToy("Гоночный автомобиль", 300);
+            Toy stickerPack = toyStore.addNewToy("Набор стикеров", 500);
+            toyStore.setWeight(stickerPack.getId(), 600);
+            toyStore.addNewToy("Пластелин", 250);
+            toyStore.addNewToy("Тамагочи", 300);
+
+            toyStore.generateToyQueue();
+            Toy nextToy;
+            while ((nextToy = toyStore.nextToy()) != null) {
+                System.out.println("Got toy: " + nextToy);
+            }
+        } catch (RuntimeException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
