0717433ffaec99ca2c1bfd69160be272a224098f
Ruslan <sonikx94@yandex.ru>
2023-02-28 23:34:30 +0300

    Write result to file

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,17 @@
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
 public class Main {
+    private static final String OUTPUT_FILE = "output.txt";
+
     public static void main(String[] args) {
         ToyStore toyStore = new ToyStore();
 
+        FileOutputStream fileStream = null;
         try {
+            System.out.println("Configure toys");
             toyStore.addNewToy("Человек-паук");
             toyStore.addNewToy("Плюшевый медведь", 200);
             toyStore.addNewToy("Гоночный автомобиль", 300);
@@ -11,13 +20,34 @@ public class Main {
             toyStore.addNewToy("Пластелин", 250);
             toyStore.addNewToy("Тамагочи", 300);
 
+            File outputFile = new File(OUTPUT_FILE);
+            outputFile.createNewFile();
+            fileStream = new FileOutputStream(outputFile, false);
+
+            System.out.println("Generate toy prizes queue...");
             toyStore.generateToyQueue();
+
+            System.out.println("Write prizes to file");
             Toy nextToy;
+            int toyNumber = 1;
             while ((nextToy = toyStore.nextToy()) != null) {
-                System.out.println("Got toy: " + nextToy);
+                fileStream.write((toyNumber + ". "+ nextToy + "\n").getBytes(StandardCharsets.UTF_8));
+                toyNumber++;
             }
+
+            System.out.println("File successfully saved!");
         } catch (RuntimeException e) {
             System.out.println(e.getMessage());
+        } catch (IOException e) {
+            System.out.println("File error: " + e);
+        } finally {
+            if (fileStream != null) {
+                try {
+                    fileStream.close();
+                } catch (IOException e) {
+                    System.out.println("Close file error: " + e);
+                }
+            }
         }
     }
 }
